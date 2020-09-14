50ec835c97cd7b0b5632caeff33182eb314adf88
lyhc <congly1311@gmail.com>
2020-09-14 11:16:10 +0700

    add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,33 @@
-import React, { Component } from 'react'
-import styled from 'styled-components'
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
+import About from './pages/About';
+import Home from './pages/Home';
 
 export default class App extends Component {
   render() {
-    return <Container>hello world</Container>
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path="/about">
+              <About></About>
+            </Route>
+            <Route path="/">
+              <Home></Home>
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    );
   }
 }
-
-const Container = styled.div`
-  font-size: 40px;
-  text-transform: capitalize;
-`
